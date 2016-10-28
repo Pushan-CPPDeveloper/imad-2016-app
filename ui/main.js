069113c3c874725d70d7892bb3ea59766a21dac0
Pushan Banerjee <pushanbjee@gmail.com>
2016-10-29 04:19:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,13 +22,26 @@ var nameInput=document.getElementById("name");
 var name=nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick=function(){
-    //request to server with name sending to it
+     // make request
+   var request=new XMLHttpRequest();
+   
+   //capture request
+   request.onreadystatechange = function(){
+                   if(request.readyState===XMLHttpRequest.DONE){
+                       if(request.status===200){
+                          var names=request.responseText;
+                          name=JSON.parse(names);
+            var list ='';
+            for(var i  =0; i< names.length; i++){
+                list += '<li>'+names[i]+'<li>' ;
+            }
+                var ul = document.getElementById("namelist");
+                ul.innerHTML=list;
+           }
+       }
+   };
+   //render variable in correct span
+   request.open('GET', 'http://pushan-cppdeveloper.imad.hasura-app.io/submit-name='+name, true);
+   request.send(null);//request to server with name sending to it
     //capture list with entry got
-var names=['name1','name2','name3','name4'];
-var list ='';
-for(var i  =0; i< names.length; i++){
-    list += '<li>'+names[i]+'<li>' ;
-}
-    var ul = document.getElementById("namelist");
-    ul.innerHTML=list;
 };
