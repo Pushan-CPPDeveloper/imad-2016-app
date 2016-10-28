9c6462c7b8268dc523396212a8eeb677234ebef0
Pushan Banerjee <pushanbjee@gmail.com>
2016-10-28 22:59:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById("firstid");
 element.innerHTML='New and interesting stuffs';
-var img = document.getElementById("madi");
+var img = document.getElementById("ju");
 var marginRight  =0;
 function moveLeft(){
     marginRight=marginRight+2;
