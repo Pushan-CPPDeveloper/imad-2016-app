dbe28cb1752bc27e4bd24fc1f9dc753caf756bfd
Pushan Banerjee <pushanbjee@gmail.com>
2016-10-28 23:25:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,19 @@ var button=document.getElementById("counter");
 var counter = 0;
 button.onclick = function() {
    // make request
+   var request=new XMLHttpRequest();
    
    //capture request
-   
+   request.onreadystatechange = function(){
+       if(request.readyState===XMLHttpRequest.DONE){
+           if(request.status===200){
+               var counter=request.responseText;
+               var span=document.getElementById("count");
+               span.innerHTML=counter.toString();
+           }
+       }
+   };
    //render variable in correct span
-   counter=counter+1;
-   var span = document.getElementById("count");
-   span.innerHTML=counter.toString();
+   request.open('GET', 'http://pushan-cppdeveloper.imad.hasura-app.io/counter', true);
+   request.send(null);
 };
