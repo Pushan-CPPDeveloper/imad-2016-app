ea1b91a71fa0f58ee3aa21e4dbba0d40fc0adb1c
Pushan Banerjee <pushanbjee@gmail.com>
2016-10-28 14:44:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,12 @@ console.log('Loaded!');
 var element=document.getElementById("firstid");
 element.innerHTML='New and interesting stuffs';
 var img = document.getElementById("madi");
+var marginRight  =0;
+function moveLeft(){
+    marginRight=marginRight+2;
+    img.style.marginRight=marginRight+'px';
+}
 img.onclick = function() {
+    var element=setInterval(moveLeft, 45);
     img.style.marginLeft = '120px';
 };
